812933f4fde2f8be4e5394b93a97ef7ddfdc0d92
DreaMinder <dreaminder7@gmail.com>
2018-06-10 22:52:26 +0300

    update deps

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,18 +1,11 @@
-let development = process.env.NODE_ENV !== 'production'
+const development = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   mode: 'spa',
   head: {
     title: '...',
     titleTemplate: '%s :: Task Manager',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'mobile-web-app-capable', content: 'yes' },
-      { name: 'apple-mobile-web-app-capable', content: 'yes' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=.9' }
-    ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: '//fonts.googleapis.com/icon?family=Material+Icons' },
       { rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Roboto:400,700,400italic' }
     ]
@@ -26,23 +19,21 @@ module.exports = {
     color: '#fff',
     background: '#333'
   },
+  manifest: { theme_color: '#02675d' },
   modules: [
     '@nuxtjs/axios',
+    '@nuxtjs/toast',
     '@nuxtjs/auth',
-    '@nuxtjs/toast'
+    '@nuxtjs/pwa'
   ],
-  build: {
-    extractCSS: true
-  },
+  build: { extractCSS: true },
   plugins: [
    '@/plugins/material.js',
    '@/plugins/muse-ui.js',
    '@/plugins/moment.js',
    '@/plugins/i18n.js'
   ],
-  toast: {
-    duration: 4000
-  },
+  toast: { duration: 4000 },
   auth: {
     resetOnError: true,
     strategies: {
@@ -55,7 +46,5 @@ module.exports = {
       }
     }
   },
-  router: {
-   middleware: ['auth']
-  }
+  router: { middleware: ['auth'] }
 }
